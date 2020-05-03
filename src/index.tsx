764b11fb017aa0ef5828b37eaab01fb073b06a93
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-03 18:46:30 +0200

    Add type script

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from './js/components/App'
+
+
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(<App />, wrapper) : false;
