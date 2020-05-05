ffc2c4e507c6ec45d57679b57a1059ba33a2308b
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-05 19:33:16 +0200

    Add Grid

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 import Form from "./Form";
 import './App.css';
+import Main from './Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Form />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <div className="item item-1">Head</div>
+      <div className="item item-2">Main</div>
+      <div className="item item-3">
+      
+      </div>
+      <div className="item item-4">Footer</div>
+    </div>
+  );
+};
+
+export default Main;
