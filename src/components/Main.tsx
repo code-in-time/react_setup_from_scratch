a6d836acc50ddbf15b07e82da62bb6d1c9e86204
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-05 19:42:52 +0200

    add side

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,12 +4,17 @@ import './Main.css';
 const Main = () => {
   return (
     <div className="Main">
-      <div className="item item-1">Head</div>
-      <div className="item item-2">Main</div>
-      <div className="item item-3">
-      
+      <div className="item-1">Head</div>
+      <div className="item-2">
+
+      <div className="wrapper">
+        <div className="side-left">side-left</div>
+        <div className="side-right">side-right</div>
       </div>
-      <div className="item item-4">Footer</div>
+
+      </div>
+      <div className="item-3">Side</div>
+      <div className="item-4">Footer</div>
     </div>
   );
 };
