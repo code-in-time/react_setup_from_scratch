e2f2f6fe4bc1a81d1d027dcf36338748c5f2599c
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-16 11:55:04 +0200

    Add cypress test

--- cypress/integration/TodoTests/home_page_spec.js
+++ cypress/integration/TodoTests/home_page_spec.js
@@ -12,8 +12,20 @@ describe('Add a Todo', () => {
   })
 
   it('should test the description input', () => {
-    cy.get('input[placeholder="Description"]').type('this is description');
-    cy.get('input[placeholder="Description"]').should('have.value','this is description')
-    cy.get('input[placeholder="Description"]').should('not.have.value','this is description')
+    cy.get('input[placeholder="Description"]').type('this is a description');
+    cy.get('input[placeholder="Description"]').should('have.value','this is a description')
+    cy.get('input[placeholder="Description"]').should('not.have.value','this is a description1')
+  })
+
+  it('should test that a Todo is added', () => {
+    cy.get('input[placeholder="Name"]').clear() .type('this is text');
+    cy.get('input[placeholder="Description"]').clear().type('this is a description');
+    cy.get('button.Button').contains('Add Todo').click()
+
+    cy.get('.TodoArea').find('.itemName').contains('this is text')
+    cy.get('.TodoArea').find('.itemDesc').contains('this is a description')
+
+
+
   })
 })
