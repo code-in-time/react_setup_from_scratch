f26dc85e9b0c78e458b295d46247628302c7a240
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-16 11:38:20 +0200

    test name and description inputs

--- cypress/integration/TodoTests/home_page_spec.js
+++ cypress/integration/TodoTests/home_page_spec.js
@@ -3,3 +3,17 @@ describe('The Home Page', () => {
     cy.visit('/') // change URL to match your dev URL
   })
 })
+
+describe('Add a Todo', () => {
+  it('should test the name input', () => {
+    cy.get('input[placeholder="Name"]').type('this is text');
+    cy.get('input[placeholder="Name"]').should('have.value','this is text')
+    cy.get('input[placeholder="Name"]').should('not.have.value','this is text1')
+  })
+
+  it('should test the description input', () => {
+    cy.get('input[placeholder="Description"]').type('this is description');
+    cy.get('input[placeholder="Description"]').should('have.value','this is description')
+    cy.get('input[placeholder="Description"]').should('not.have.value','this is description')
+  })
+})
