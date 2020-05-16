9c2c93f2e38ff35ecc38edad4ce0f3b4229e692e
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-16 17:25:09 +0200

    Allow css and scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
+import './style.scss';
 
 
 const wrapper = document.getElementById("container");
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,10 @@ module.exports = env => {
           ]
         },
         {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         }
