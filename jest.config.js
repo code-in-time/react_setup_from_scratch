349947659b06de931e92e6aa65230a6a3fed6bf2
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-18 20:05:22 +0200

    jest test now run

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  "roots": [
+  roots: [
     "<rootDir>/src"
   ],
-  "testMatch": [
+  testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  transform: {
+    "^.+\\.(ts|tsx|js)$": "ts-jest"
   },
   verbose: true,
   preset: 'ts-jest',
-  // setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  testEnvironment: 'node'
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/src/__mocks__/styleMock.js"
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  verbose: true
 }
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import App from '../components/App'
-// import {shallow} from 'enzyme'
+import App from '../components/App'
+import {shallow} from 'enzyme'
 
 describe(' Test the App component', () => {
 
   it('test if the App component loads', () => {
-    // let wrapper = shallow(<App />);
+    let wrapper = shallow(<App />);
     expect(1).toEqual(1)
   })
 
