e068030680baa68197a575439a3d4e7546b40c13
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-18 20:17:53 +0200

    Add first test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import App from '../components/App'
+import Main from '../components/Main';
 import {shallow} from 'enzyme'
 
 describe(' Test the App component', () => {
 
-  it('test if the App component loads', () => {
+  it('test if the App component loads and if it contains the Main component', () => {
     let wrapper = shallow(<App />);
+    expect(wrapper.find(Main).length).toBe(1)
     expect(1).toEqual(1)
   })
 
